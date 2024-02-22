762e52e34991605014f36acbd198982b6b39e515
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-02-22 19:21:40 +0330

    Add basic checks for CheckingAccount

--- src/main/java/com/campus/banking/Main.java
+++ src/main/java/com/campus/banking/Main.java
@@ -1,8 +1,10 @@
 package com.campus.banking;
 
 import com.campus.banking.model.BankAccount;
+import com.campus.banking.model.CheckingAccount;
 import com.campus.banking.service.BankAccountService;
 import com.campus.banking.service.BankAccountServiceImpl;
+import com.campus.banking.service.CheckingAccountService;
 
 public class Main {
     public static void main(String[] args) {
@@ -15,5 +17,18 @@ public class Main {
         service.deposit(account, 10.0);
 
         System.out.println(account.getBalance());
+
+
+        CheckingAccount checkingAccount = new CheckingAccount("1001", "CheckingTester", 10.0d, 10.0d, 0.0d);
+        CheckingAccountService checkingAccountService = new CheckingAccountService();
+
+        checkingAccountService.withdraw(checkingAccount, 5.0d);
+        System.out.println(checkingAccount.getBalance());
+        
+        checkingAccountService.withdraw(checkingAccount, 10.0d);
+        System.out.println(checkingAccount.getBalance());
+
+        checkingAccountService.withdraw(checkingAccount, 5.0d);
+        System.out.println(checkingAccount.getBalance());
     }
 }
