4aa0ec5f8386f21eb069b5b2f34b4d056306e393
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-02-22 15:43:52 +0330

    Add main class for basic checks

--- src/main/java/com/campus/banking/Main.java
+++ src/main/java/com/campus/banking/Main.java
@@ -1,7 +1,19 @@
 package com.campus.banking;
 
+import com.campus.banking.model.BankAccount;
+import com.campus.banking.service.BankAccountService;
+import com.campus.banking.service.BankAccountServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount account = BankAccount.builder()
+                .accountHolderName("Tester")
+                .accountNumber("1000")
+                .amount(0.0d)
+                .build();
+        BankAccountService service = new BankAccountServiceImpl();
+        service.deposit(account, 10.0);
+
+        System.out.println(account.getAmount());
     }
 }
