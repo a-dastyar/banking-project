1e9f5ac2347d668382947e100508fa5ba7fd285d
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-02-22 19:26:46 +0330

    Add CheckingAccountService

---
+++ src/main/java/com/campus/banking/service/CheckingAccountService.java
@@ -0,0 +1,35 @@
+package com.campus.banking.service;
+
+import com.campus.banking.exception.InsufficientFundsException;
+import com.campus.banking.model.BankAccount;
+import com.campus.banking.model.CheckingAccount;
+
+public class CheckingAccountService extends BankAccountServiceImpl {
+    @Override
+    public void deposit(BankAccount account, double amount) {
+        super.deposit(account, amount);
+    }
+
+    @Override
+    public void withdraw(BankAccount account, double amount) {
+        if (!(account instanceof CheckingAccount)) {
+            // TODO: make an exception type
+            return;
+        }
+
+        CheckingAccount checkingAccount = (CheckingAccount)account;
+
+        if (checkingAccount.getOverDraft() > 0.0d) {
+            throw new InsufficientFundsException("Can not withdraw while you have debt");
+        }
+
+        double balance = checkingAccount.getBalance();
+        if (amount > balance &&
+            amount <= balance + checkingAccount.getOverDraftLimit()) {
+            checkingAccount.setOverDraft(amount - balance);
+            amount = balance;
+        }
+
+        super.withdraw(checkingAccount, amount);
+    }
+}
