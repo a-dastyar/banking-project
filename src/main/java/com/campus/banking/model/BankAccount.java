71fdf3c14bd77e2870a9cfb0564428437b1b02b4
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-02-25 22:33:09 +0330

    Fix annotations for lombok and jackson

--- src/main/java/com/campus/banking/model/BankAccount.java
+++ src/main/java/com/campus/banking/model/BankAccount.java
@@ -1,19 +1,36 @@
 package com.campus.banking.model;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonTypeName;
+
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.With;
+import lombok.experimental.SuperBuilder;
 
 @Data
 @With
-@Builder
+@SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
+@JsonSubTypes({
+        @Type(value = SavingAccount.class, name = "saving"),
+        @Type(value = CheckingAccount.class, name = "checking")
+})
+@JsonTypeName("normal")
 public class BankAccount {
+
+    @EqualsAndHashCode.Include
     private String accountNumber;
+
     private String accountHolderName;
+
     private double balance;
 
     public BankAccount(String accountNumber, String accountHolderName) {
--- src/main/java/com/campus/banking/model/SavingAccount.java
+++ src/main/java/com/campus/banking/model/SavingAccount.java
@@ -1,24 +1,29 @@
 package com.campus.banking.model;
 
-import com.campus.banking.utils.InterestType;
+import com.fasterxml.jackson.annotation.JsonTypeName;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.With;
+import lombok.experimental.SuperBuilder;
 
-// TODO: search about @Builder
-@AllArgsConstructor
 @Data
-@EqualsAndHashCode(callSuper = false)
 @With
+@SuperBuilder
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
 public class SavingAccount extends BankAccount {
+
     public static final double INTEREST_RATE = 0.1d;
 
-    private InterestType interestType;
+    private InterestPeriod interestType;
+    
     private double minimumBalance;
 
-    public SavingAccount(String accountNumber, String accountHolderName, double balance, InterestType interestType, double minimumBalance) {
+    public SavingAccount(String accountNumber, String accountHolderName, double balance, InterestPeriod interestType, double minimumBalance) {
         super(accountNumber, accountHolderName, balance);
         this.interestType = interestType;
         this.minimumBalance = minimumBalance;
