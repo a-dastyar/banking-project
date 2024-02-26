f1c33cd62b18d7cf4a330b4a0474ac14b2f5c9a3
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-02-26 10:58:43 +0330

    Add basic validation

--- src/main/java/com/campus/banking/persistence/BankAccountDAOImpl.java
+++ src/main/java/com/campus/banking/persistence/BankAccountDAOImpl.java
@@ -3,6 +3,7 @@ package com.campus.banking.persistence;
 import java.util.List;
 import java.util.Optional;
 
+import com.campus.banking.exception.InvalidAccountException;
 import com.campus.banking.model.BankAccount;
 
 import lombok.AllArgsConstructor;
@@ -14,16 +15,31 @@ public class BankAccountDAOImpl implements BankAccountDAO {
 
     @Override
     public void add(BankAccount account) {
+        validate(account);
         db.add(account);
+    }
+
+    private void validate(BankAccount account) {
+        if (account == null || account.getAccountNumber() == null || account.getAccountNumber().isEmpty()) {
+            throw new InvalidAccountException();
+        }
     }
 
     @Override
     public Optional<BankAccount> findByAccountNumber(String accountNumber) {
+        validateAccountNumber(accountNumber);
         return Optional.ofNullable(db.get(accountNumber));
+    }
+
+    private void validateAccountNumber(String accountNumber) {
+        if (accountNumber == null || accountNumber.isEmpty()) {
+            throw new IllegalArgumentException("Account number can not be null or blank");
+        }
     }
 
     @Override
     public void removeByAccountNumber(String accountNumber) {
+        validateAccountNumber(accountNumber);
         db.remove(accountNumber);
     }
 
