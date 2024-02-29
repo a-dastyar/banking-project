30771dece763f4a6e7c3397714c45e25e605d92e
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-02-29 15:52:32 +0330

    Make SavingAccountService thread-safe and add concurrent apply interest feat

--- src/main/java/com/campus/banking/service/SavingAccountService.java
+++ src/main/java/com/campus/banking/service/SavingAccountService.java
@@ -1,7 +1,10 @@
 package com.campus.banking.service;
 
+import java.util.List;
+
 import com.campus.banking.model.SavingAccount;
 
 public interface SavingAccountService extends BankAccountService {
     void applyInterest(SavingAccount account);
+    void applyInterest(List<SavingAccount> accounts);
 }
--- src/main/java/com/campus/banking/service/SavingAccountServiceImpl.java
+++ src/main/java/com/campus/banking/service/SavingAccountServiceImpl.java
@@ -1,5 +1,8 @@
 package com.campus.banking.service;
 
+import java.util.List;
+import java.util.concurrent.Executors;
+
 import com.campus.banking.exception.InvalidAccountTypeException;
 import com.campus.banking.exception.InvalidTransactionException;
 import com.campus.banking.model.BankAccount;
@@ -12,20 +15,42 @@ public class SavingAccountServiceImpl extends BankAccountServiceImpl implements 
             throw new InvalidAccountTypeException("BankAccount type must be from type SavingAccount");
         }
 
-        SavingAccount savingAccount = (SavingAccount)account;
+        SavingAccount savingAccount = (SavingAccount) account;
 
-        double maximum_withdraw = savingAccount.getBalance() - savingAccount.getMinimumBalance();
-        if (amount > maximum_withdraw) {
-            throw new InvalidTransactionException("Can not withdraw more than " + maximum_withdraw);
+        try (var lock = savingAccount.getLock().lock()) {
+            double maximum_withdraw = savingAccount.getBalance() - savingAccount.getMinimumBalance();
+            if (amount > maximum_withdraw) {
+                throw new InvalidTransactionException("Can not withdraw more than " + maximum_withdraw);
+            }
+
+            super.withdraw(savingAccount, amount);
         }
+    }
 
-        super.withdraw(savingAccount, amount);
+    @Override
+    public void deposit(BankAccount account, double amount) {
+        if (!(account instanceof SavingAccount)) {
+            throw new InvalidAccountTypeException("BankAccount type must be from type SavingAccount");
+        }
+        super.deposit(account, amount);
     }
 
     @Override
     public void applyInterest(SavingAccount account) {
-        double interest = account.getBalance() * SavingAccount.INTEREST_RATE;
 
-        super.deposit(account, interest);
+        try (var lock = account.getLock().lock()) {
+            double interest = account.getBalance() * SavingAccount.INTEREST_RATE;
+            
+            super.deposit(account, interest);
+        }
+    }
+
+    @Override
+    public void applyInterest(List<SavingAccount> accounts) {
+        try (var executors = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (var account : accounts) {
+                executors.submit(() -> this.applyInterest(account));
+            }
+        }
     }
 }
