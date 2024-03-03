3d3500f3d565cbf8a521d98d31fe09b031ad5651
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-03-03 23:13:52 +0330

    Refactor getDeprecatedMethods using Stream

--- src/main/java/com/campus/banking/utils/ReflectionUtil.java
+++ src/main/java/com/campus/banking/utils/ReflectionUtil.java
@@ -1,30 +1,21 @@
 package com.campus.banking.utils;
 
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ReflectionUtil {
 
     public static List<DeprecatedMethodData> getDeprecatedMethods(Class<?>... classes) {
-        List<DeprecatedMethodData> datas = new ArrayList<>();
-
-        for (var clazz : classes) {
-            // TODO: check for clazz.getMethods() either?
-            Method[] methods = clazz.getDeclaredMethods();
-
-            for (var method : methods) {
-                DeprecatedMethod annotation = method.getAnnotation(DeprecatedMethod.class);
-
-                if (annotation != null) {
-                    String reason = annotation.reason();
-                    String replacement = annotation.replacement();
-                    DeprecatedMethodData data = new DeprecatedMethodData(method, reason, replacement);
-
-                    datas.add(data);
-                }
-            }
-        }
-        return datas;
+        return Stream.of(classes)
+        .<Method> mapMulti((clazz, consumer) -> {
+            Stream.of(clazz.getDeclaredMethods()).forEach(consumer);
+        })
+        .filter((method) -> method.isAnnotationPresent(DeprecatedMethod.class))
+        .map((method) -> {
+            var annotation = method.getAnnotation(DeprecatedMethod.class);
+            return new DeprecatedMethodData(method, annotation.reason(), annotation.replacement());
+        })
+        .toList();
     }
 }
