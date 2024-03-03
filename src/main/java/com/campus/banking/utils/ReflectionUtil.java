044168b363c0332de35576244c007e9e2d57fc28
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-03-03 20:52:21 +0330

    Implement ReflectionUtil getDeprecatedMethods

--- src/main/java/com/campus/banking/utils/ReflectionUtil.java
+++ src/main/java/com/campus/banking/utils/ReflectionUtil.java
@@ -1,11 +1,30 @@
 package com.campus.banking.utils;
 
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReflectionUtil {
 
     public static List<DeprecatedMethodData> getDeprecatedMethods(Class<?>... classes) {
-        // TODO implement
-        throw new UnsupportedOperationException();
+        List<DeprecatedMethodData> datas = new ArrayList<>();
+
+        for (var clazz : classes) {
+            // TODO: check for clazz.getMethods() either?
+            Method[] methods = clazz.getDeclaredMethods();
+
+            for (var method : methods) {
+                DeprecatedMethod annotation = method.getAnnotation(DeprecatedMethod.class);
+
+                if (annotation != null) {
+                    String reason = annotation.reason();
+                    String replacement = annotation.replacement();
+                    DeprecatedMethodData data = new DeprecatedMethodData(method, reason, replacement);
+
+                    datas.add(data);
+                }
+            }
+        }
+        return datas;
     }
 }
