2b2d44e9e1945eb6d86d69394d60eae6c8d44343
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-03-08 17:17:44 +0330

    add hibernate persistence annotations to BankAccount model

--- src/main/java/com/campus/banking/model/BankAccount.java
+++ src/main/java/com/campus/banking/model/BankAccount.java
@@ -6,6 +6,15 @@ import com.campus.banking.utils.AutoCloseableLock;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
+
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
 
@@ -29,18 +38,32 @@ import lombok.experimental.SuperBuilder;
         @Type(value = CheckingAccount.class, name = "checking")
 })
 @JsonTypeName("normal")
+@Entity
+@Table(name = "bank_accounts")
 public class BankAccount {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @JsonIgnore
     @ToString.Exclude
+    @Transient
     private final AutoCloseableLock lock = new AutoCloseableLock(new ReentrantLock());
 
     @EqualsAndHashCode.Include
+    @Column(name = "account_number")
     private String accountNumber;
 
+    @Column(name = "account_holder_name")
     private String accountHolderName;
 
     private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double balance) {
+        // TODO: another work around could be using @RequiredArgsConstructor and @NonNull
+        this(null, accountNumber, accountHolderName, balance);
+    }
 
     public BankAccount(String accountNumber, String accountHolderName) {
         this(accountNumber, accountHolderName, 0.0d);
