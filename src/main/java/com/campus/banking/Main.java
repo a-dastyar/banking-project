1a13502f74e58376b10a94560b4d029f767a4b59
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-03-08 17:18:46 +0330

    add basic check to Main file

--- src/main/java/com/campus/banking/Main.java
+++ src/main/java/com/campus/banking/Main.java
@@ -1,44 +1,45 @@
 package com.campus.banking;
 
-import com.campus.banking.exception.LoadFailureException;
-import com.campus.banking.exception.SaveFailureException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
 import com.campus.banking.model.BankAccount;
-import com.campus.banking.model.InterestPeriod;
-import com.campus.banking.model.SavingAccount;
-import com.campus.banking.persistence.BankAccountDAO;
-import com.campus.banking.persistence.BankAccountDAOImpl;
-import com.campus.banking.persistence.Database;
-import com.campus.banking.persistence.DatabaseImpl;
-import com.campus.banking.persistence.SavingAccountDAOImpl;
 
 public class Main {
+
+	private static SessionFactory sessionFactory;
+
 	public static void main(String[] args) {
-		var account = BankAccount.builder()
-				.accountHolderName("Tester")
-				.accountNumber("1000")
-				.balance(0.0d)
-				.build();
-		var saving = SavingAccount.builder()
-				.accountNumber("2000")
-				.interestPeriod(InterestPeriod.MONTHLY).build();
+		setUp();
+		sessionFactory.inTransaction(session -> {
+			session.persist(new BankAccount().withBalance(200.0d));
+		});
+		sessionFactory.inTransaction(session -> {
+			session.createSelectionQuery("from bank_accounts", BankAccount.class)   
+					.getResultList()   
+					.forEach(account -> System.out.println("BankAccount (" + account.getBalance() + ") : " + account.getId()));
+		});
+	}
 
-		Database db = DatabaseImpl.INSTANCE;
-		BankAccountDAO<BankAccount> bankAccountDAO = new BankAccountDAOImpl(db);
-		BankAccountDAO<SavingAccount> savingAccountDAO = new SavingAccountDAOImpl(db);
-
-		bankAccountDAO.add(account);
-		savingAccountDAO.add(saving);
-
+	private static void setUp() {
+		// A SessionFactory is set up once for an application!
+		final StandardServiceRegistry registry =
+				new StandardServiceRegistryBuilder()
+						.build();
 		try {
-			db.persist();
-			db.load();
-		} catch (SaveFailureException e) {
-			System.err.println("Failed to save to file");
-			e.printStackTrace();
-		} catch (LoadFailureException e) {
-			System.err.println("Failed to read from file");
-			e.printStackTrace();
+			sessionFactory =
+					new MetadataSources(registry)
+							.addAnnotatedClass(BankAccount.class)
+							.buildMetadata()
+							.buildSessionFactory();
 		}
-		bankAccountDAO.list().forEach(System.out::println);
+		catch (Exception e) {
+			// The registry would be destroyed by the SessionFactory, but we
+			// had trouble building the SessionFactory so destroy it manually.
+			e.printStackTrace();
+			StandardServiceRegistryBuilder.destroy(registry);
+		}
 	}
 }
