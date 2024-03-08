530104ce0c5a19c6980a0a0f2ee840fa276dd47f
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-03-08 21:55:10 +0330

    Add sample code

--- src/main/java/com/campus/banking/Main.java
+++ src/main/java/com/campus/banking/Main.java
@@ -1,44 +1,71 @@
 package com.campus.banking;
 
-import com.campus.banking.exception.LoadFailureException;
-import com.campus.banking.exception.SaveFailureException;
+import java.io.IOException;
+import java.sql.SQLException;
+
 import com.campus.banking.model.BankAccount;
+import com.campus.banking.model.CheckingAccount;
 import com.campus.banking.model.InterestPeriod;
 import com.campus.banking.model.SavingAccount;
-import com.campus.banking.persistence.BankAccountDAO;
 import com.campus.banking.persistence.BankAccountDAOImpl;
-import com.campus.banking.persistence.Database;
+import com.campus.banking.persistence.CheckingAccountDAOImpl;
 import com.campus.banking.persistence.DatabaseImpl;
+import com.campus.banking.persistence.HikariDatasource;
 import com.campus.banking.persistence.SavingAccountDAOImpl;
+import com.campus.banking.persistence.TransactionDAOImpl;
+import com.campus.banking.service.BankAccountServiceImpl;
+import com.campus.banking.service.CheckingAccountServiceImpl;
+import com.campus.banking.service.SavingAccountServiceImpl;
 
 public class Main {
-	public static void main(String[] args) {
+
+	public static void main(String[] args) throws SQLException, IOException {
 		var account = BankAccount.builder()
 				.accountHolderName("Tester")
 				.accountNumber("1000")
 				.balance(0.0d)
 				.build();
+
 		var saving = SavingAccount.builder()
-				.accountNumber("2000")
+				.accountHolderName("Saver")
+				.accountNumber("2002")
+				.balance(1000.0)
+				.interestRate(10.0)
+				.minimumBalance(300)
 				.interestPeriod(InterestPeriod.MONTHLY).build();
 
-		Database db = DatabaseImpl.INSTANCE;
-		BankAccountDAO<BankAccount> bankAccountDAO = new BankAccountDAOImpl(db);
-		BankAccountDAO<SavingAccount> savingAccountDAO = new SavingAccountDAOImpl(db);
+		var checking = CheckingAccount.builder()
+				.accountHolderName("Checker")
+				.accountNumber("2003")
+				.balance(1000.0)
+				.overDraftLimit(100.0)
+				.debt(0.0).build();
 
-		bankAccountDAO.add(account);
-		savingAccountDAO.add(saving);
+		var db = new DatabaseImpl(HikariDatasource.INSTANCE);
+		db.clear();
+		var dao = new BankAccountDAOImpl(db);
+		var trxDao = new TransactionDAOImpl(db);
+		var bankService = new BankAccountServiceImpl(dao, trxDao);
 
-		try {
-			db.persist();
-			db.load();
-		} catch (SaveFailureException e) {
-			System.err.println("Failed to save to file");
-			e.printStackTrace();
-		} catch (LoadFailureException e) {
-			System.err.println("Failed to read from file");
-			e.printStackTrace();
-		}
-		bankAccountDAO.list().forEach(System.out::println);
+		bankService.add(account);
+		bankService.deposit(account.getAccountNumber(), 2000);
+		bankService.withdraw(account.getAccountNumber(), 1000);
+		var found = bankService.getByAccountNumber(account.getAccountNumber());
+		System.out.println(found);
+
+		var checkingDao = new CheckingAccountDAOImpl(db, dao);
+		var checkingService = new CheckingAccountServiceImpl(checkingDao, trxDao);
+
+		checkingService.add(checking);
+		checkingService.deposit(checking.getAccountNumber(), 2000.0);
+		checkingService.withdraw(checking.getAccountNumber(), 1000.0);
+		var foundChecking = checkingService.getByAccountNumber(checking.getAccountNumber());
+		System.out.println(foundChecking);
+
+		var savingDao = new SavingAccountDAOImpl(db, dao);
+		var savingService = new SavingAccountServiceImpl(savingDao, trxDao);
+		savingService.add(saving);
+		var foundSaving = savingService.getByAccountNumber(saving.getAccountNumber());
+		System.out.println(foundSaving);
 	}
 }
