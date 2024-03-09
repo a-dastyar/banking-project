20376d2d171247487147cfccea4634566ca15daa
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-03-09 20:13:45 +0330

    Add model for transction

---
+++ src/main/java/com/campus/banking/model/Transaction.java
@@ -0,0 +1,51 @@
+package com.campus.banking.model;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Index;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.With;
+
+@Data
+@With
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "transactions", indexes = {
+        @Index(name = "trx_bnk_acc_id_idx", columnList = "bank_account_id", unique = false) })
+public class Transaction implements BaseModel<Long> {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "bank_account_id", nullable = false)
+    private BankAccount account;
+
+    @Column(name = "amount")
+    private double amount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "type")
+    private TransactionType type;
+
+    @Column(name = "date")
+    private LocalDateTime date;
+}
