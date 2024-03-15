36931250e93579b14ceb0196f575d34669ba3e29
AmentBRT <50689884+AmentBRT@users.noreply.github.com>
2024-03-15 14:37:29 +0330

    Add Servlets for Deposit, Withdraw and SumBalance

---
+++ src/main/java/com/campus/banking/controller/BankAccountWithdrawServlet.java
@@ -0,0 +1,47 @@
+package com.campus.banking.controller;
+
+import java.io.IOException;
+import java.util.Optional;
+
+import com.campus.banking.model.BankAccount;
+import com.campus.banking.service.BankAccountService;
+
+import jakarta.inject.Inject;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@WebServlet("/bank_account_withdraw")
+public class BankAccountWithdrawServlet extends HttpServlet {
+
+    private BankAccountService<BankAccount> service;
+
+    @Inject
+    public void initialize(BankAccountService<BankAccount> service) {
+        this.service = service;
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        log.debug("POST");
+        String accountNumber = req.getParameter("account_number");
+        var amount = Optional.ofNullable(req.getParameter("withdraw_amount"))
+                .filter(str -> str.chars().allMatch(this::isNum))
+                .map(Double::valueOf)
+                .filter(i -> i >= 0.0d)
+                .orElse(0.0d);
+        this.service.withdraw(accountNumber, amount);
+
+        BankAccount account = this.service.getByAccountNumber(accountNumber);
+        req.setAttribute("account", account);
+        req.getRequestDispatcher("/views/bank_account.jsp").forward(req, resp);
+    }
+
+    private boolean isNum(int codePonit) {
+        return Character.isDigit(codePonit) || codePonit == ".".codePointAt(0);
+    }
+}
