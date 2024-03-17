19882192193193f8417d808850d1cdd4e35a2f81
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-03-17 11:27:24 +0330

    Add unit-test for HashingService

---
+++ src/test/java/com/campus/banking/service/HashServiceTest.java
@@ -0,0 +1,34 @@
+package com.campus.banking.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+
+public class HashServiceTest {
+
+    private HashService service = new Aragon2HashService();
+
+    @Test
+    void hashOf_withRawString_shouldHash() {
+        var input = "Test";
+        var hash = service.hashOf(input);
+        assertThat(input).isNotEqualTo(hash);
+    }
+
+    @Test
+    void matches_withMatchingHash_shouldReturnTrue() {
+        var input = "Test";
+        var hash = service.hashOf(input);
+        var matches = service.matches(input, hash);
+        assertThat(matches).isTrue();
+    }
+
+    @Test
+    void matches_withNonMatchingHash_shouldReturnFalse() {
+        var input = "Test";
+        var hash = service.hashOf(input);
+        var matches = service.matches("Test2", hash);
+        assertThat(matches).isFalse();
+    }
+}
