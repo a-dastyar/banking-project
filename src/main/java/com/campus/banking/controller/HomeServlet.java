7dc6d10cb781f9d4619f7d4c22d0d790e6194dd8
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-03-17 11:34:11 +0330

    Add EntityManager to HomeServlet for initiating DDL(s) + fix wrong jsp file address

--- src/main/java/com/campus/banking/controller/HomeServlet.java
+++ src/main/java/com/campus/banking/controller/HomeServlet.java
@@ -2,16 +2,27 @@ package com.campus.banking.controller;
 
 import java.io.IOException;
 
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 
-@WebServlet("/")
+@Slf4j
+@WebServlet("")
 public class HomeServlet extends HttpServlet {
+
+    @Inject
+    public void initialize(EntityManager em) {
+        log.debug("Setting up database");
+        em.isOpen();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("/views/home.jsp").forward(req, resp);
+        req.getRequestDispatcher("/views/pages/home.jsp").forward(req, resp);
     }
 }
