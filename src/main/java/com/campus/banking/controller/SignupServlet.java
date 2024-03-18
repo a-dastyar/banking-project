c985c5d64f20c3f386d085ef531ab0ab06a8243c
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-03-18 20:12:43 +0330

    Fix spacing

--- src/main/java/com/campus/banking/controller/SignupServlet.java
+++ src/main/java/com/campus/banking/controller/SignupServlet.java
@@ -8,7 +8,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/signup")
 public class SignupServlet  extends HttpServlet {
 
