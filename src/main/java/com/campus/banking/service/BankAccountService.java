6f5a956a43dbcc4db693e80d3645fa5f4e1b5e8f
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-03-18 19:50:08 +0330

    Add new methods to BankAccountService + changes related to user field added to model

--- src/main/java/com/campus/banking/service/BankAccountService.java
+++ src/main/java/com/campus/banking/service/BankAccountService.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 import java.util.Map;
 
 import com.campus.banking.model.BankAccount;
+import com.campus.banking.model.Transaction;
 import com.campus.banking.model.User;
+import com.campus.banking.persistence.Page;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -15,23 +17,25 @@ import jakarta.validation.constraints.PositiveOrZero;
 public interface BankAccountService<T extends BankAccount> {
 
     public static BankAccount toBankAccount(Map<String, String[]> properties) {
-        var userId = Arrays.stream(properties.get("username")).findFirst().orElse(null);
+        var username = Arrays.stream(properties.get("username")).findFirst().orElse(null);
         return BankAccount.builder()
                 .accountNumber(Arrays.stream(properties.get("account_number")).findFirst().orElse(null))
-                .accountHoler(User.builder().id(userId).build())
+                .accountHolder(User.builder().username(username).build())
                 .balance(Arrays.stream(properties.get("balance")).findFirst().map(Double::valueOf).orElse(0.0d))
                 .build();
     }
 
     void add(@NotNull @Valid T account);
 
-    @NotNull
+    Page<T> getPage(@Positive int page);
+
+    Page<Transaction> getTransactions(@NotNull @NotBlank String accountNumber, @Positive int page);
+
     T getByAccountNumber(@NotNull @NotBlank String accountNumber);
 
     void deposit(@NotNull @NotBlank String accountNumber, @Positive double amount);
 
     void withdraw(@NotNull @NotBlank String accountNumber, @Positive double amount);
 
-    @PositiveOrZero
     double sumBalanceHigherThan(@PositiveOrZero double min);
 }
