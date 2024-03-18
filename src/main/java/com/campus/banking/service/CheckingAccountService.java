191e0a38d6912e942e49b5eb38db97f7bd1f6b5f
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-03-18 19:52:49 +0330

    Remove saving prefix from submited from keys in CheckingAccountService + fix typo

--- src/main/java/com/campus/banking/service/CheckingAccountService.java
+++ src/main/java/com/campus/banking/service/CheckingAccountService.java
@@ -11,11 +11,12 @@ public interface CheckingAccountService extends BankAccountService<CheckingAccou
     public static CheckingAccount toCheckingAccount(Map<String, String[]> properties) {
         var username = Arrays.stream(properties.get("username")).findFirst().orElse(null);
         return CheckingAccount.builder()
-                .accountNumber(Arrays.stream(properties.get("checking_account_number")).findFirst().orElse(null))
-                .accountHoler(User.builder().username(username).build())
-                .balance(Arrays.stream(properties.get("checking_balance")).findFirst().map(Double::valueOf).orElse(0.0d))
-                .overDraftLimit(Arrays.stream(properties.get("checking_over_draft_limit")).findFirst().map(Double::valueOf).orElse(0.0d))
-                .debt(Arrays.stream(properties.get("checking_debt")).findFirst().map(Double::valueOf).orElse(0.0d))
+                .accountNumber(Arrays.stream(properties.get("account_number")).findFirst().orElse(null))
+                .accountHolder(User.builder().username(username).build())
+                .balance(Arrays.stream(properties.get("balance")).findFirst().map(Double::valueOf).orElse(0.0d))
+                .overdraftLimit(
+                        Arrays.stream(properties.get("overdraft_limit")).findFirst().map(Double::valueOf).orElse(0.0d))
+                .debt(Arrays.stream(properties.get("debt")).findFirst().map(Double::valueOf).orElse(0.0d))
                 .build();
     }
 }
