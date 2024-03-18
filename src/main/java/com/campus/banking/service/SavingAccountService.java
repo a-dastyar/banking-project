3e2b9bfbee1d799d04de4c2ff31e59b0ddacc9a3
Alireza Dastyar <alireza.dastyar4+1@gmail.com>
2024-03-18 19:52:16 +0330

    Remove saving prefix from submited from keys in SavingAccount + fix typo

--- src/main/java/com/campus/banking/service/SavingAccountService.java
+++ src/main/java/com/campus/banking/service/SavingAccountService.java
@@ -7,21 +7,24 @@ import com.campus.banking.model.InterestPeriod;
 import com.campus.banking.model.SavingAccount;
 import com.campus.banking.model.User;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 public interface SavingAccountService extends BankAccountService<SavingAccount> {
 
     public static SavingAccount toSavingAccount(Map<String, String[]> properties) {
         var username = Arrays.stream(properties.get("username")).findFirst().orElse(null);
         return SavingAccount.builder()
-                .accountNumber(Arrays.stream(properties.get("saving_account_number")).findFirst().orElse(null))
-                .accountHoler(User.builder().username(username).build())
-                .balance(Arrays.stream(properties.get("saving_balance")).findFirst().map(Double::valueOf).orElse(0.0d))
-                .interestRate(Arrays.stream(properties.get("saving_interest_rate")).findFirst().map(Double::valueOf).orElse(0.0d))
-                .interestPeriod(Arrays.stream(properties.get("saving_interest_period")).findFirst().map(InterestPeriod::valueOf).orElse(null))
-                .minimumBalance(Arrays.stream(properties.get("saving_minimum_balance")).findFirst().map(Double::valueOf).orElse(0.0d))
+                .accountNumber(Arrays.stream(properties.get("account_number")).findFirst().orElse(null))
+                .accountHolder(User.builder().username(username).build())
+                .balance(Arrays.stream(properties.get("balance")).findFirst().map(Double::valueOf).orElse(0.0d))
+                .interestRate(Arrays.stream(properties.get("interest_rate")).findFirst().map(Double::valueOf).orElse(0.0d))
+                .interestPeriod(Arrays.stream(properties.get("interest_period")).findFirst().map(InterestPeriod::valueOf).orElse(null))
+                .minimumBalance(Arrays.stream(properties.get("minimum_balance")).findFirst().map(Double::valueOf).orElse(0.0d))
                 .build();
     }
 
-    void applyInterest(String accountNumber);
+    void applyInterest(@NotNull @NotBlank String accountNumber);
 
     void applyInterest();
 }
